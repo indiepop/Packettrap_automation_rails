88cbd55d05a5a0bdf14e472c0eb53898f4b0f292
jyang2 <josh.yangzhuo@gmail.com>
2012-06-04 11:23:42 +0800

    Signed-off-by: jyang2 <josh.yangzhuo@gmail.com>

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PackettrapAutomationRails::Application.config.secret_token = 'a7b38249bf1fd744b6e9428babb07c358ab6be210ece6bc1311b823d684b15df892a5403b1e17e7267d5ce9442b71295eb1e3b8c23b570f0dedd66c8a92a8650'
---
+++ features/step_definitions/login_steps.rb
@@ -0,0 +1,22 @@
+require "douban_core/Initialization"
+
+Given /^I Open main page using (#{USING_CREDENTIAL})$/ do  |table|
+
+  unless $browser
+    Initialization.start
+  end                                                                    #initial browser
+
+  login = $browser.is_element_present(logout='//div[@class="top-nav-info"]/a[3]')
+  if login
+    $browser.find_element(:xpath,logout).click
+  end                                                                    #if already login, then click out.
+
+  table.rows_hash.each do |key,value|
+    step "I type '#{value}' in '#{key}' field"
+  end                                                                    #set the username and password
+
+    $browser.find_element(:xpath => "//input[@class='bn-submit']").click
+end
+
+
+
